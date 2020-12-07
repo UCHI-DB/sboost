7578e8c3b29ea9b864e194a04c5cd531a9dab2dd
Hao Jiang <harperjiang@msn.com>
2020-12-06 23:28:39 -0600

    Benchmark shows sboost based binary search is faster

--- sboost_benchmark.cc
+++ sboost_benchmark.cc
@@ -54,3 +54,63 @@ BENCHMARK_F(SboostBenchmark, Template)(benchmark::State &state) {
     }
 }
 
+int32_t binary_search(vector<uint32_t> data, int target) {
+    auto begin = 0;
+    auto end = data.size() - 1;
+    while (begin < end) {
+        auto current = (begin + end + 1) / 2;
+        if (data[current] < target) {
+            begin = current;
+        } else if (data[current] == target) {
+            return current;
+        } else {
+            end = current - 1;
+        }
+    }
+    return begin;
+}
+
+class SearchBenchmark : public benchmark::Fixture {
+protected:
+    vector<uint32_t> data_;
+    uint8_t *output_;
+    uint32_t numEntry_;
+    uint8_t bitWidth_;
+    uint64_t *res_;
+    int32_t result_;
+public:
+
+    SearchBenchmark() {
+        numEntry_ = 100000;
+        bitWidth_ = 17;
+        output_ = (uint8_t *) malloc(sizeof(uint32_t) * numEntry_);
+        res_ = (uint64_t *) malloc(sizeof(uint64_t) * (numEntry_ + 1) / 64);
+
+        for (uint32_t i = 0; i < numEntry_; ++i) {
+            data_.push_back(i);
+        }
+        ::sboost::byteutils::bitpack(data_.data(), numEntry_, bitWidth_, output_);
+    }
+
+    virtual ~SearchBenchmark() {
+        free(output_);
+        free(res_);
+    }
+};
+
+BENCHMARK_F(SearchBenchmark, STL)(benchmark::State &state) {
+    for (auto _: state) {
+        for (int i = 0; i < 100000; ++i) {
+            result_ = binary_search(data_, i);
+        }
+    }
+}
+
+BENCHMARK_F(SearchBenchmark, SBoost)(benchmark::State &state) {
+    for (auto _ : state) {
+        for (int i = 0; i < 100000; ++i) {
+            sboost::SortedBitpack sbp(bitWidth_, i);
+            result_ = sbp.equal(output_, numEntry_);
+        }
+    }
+}
